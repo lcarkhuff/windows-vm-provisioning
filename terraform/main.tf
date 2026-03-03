6a0a7838fb79c0a08430c8604fb868a16f628a38
lcarkhuff <138237342+lcarkhuff@users.noreply.github.com>
2026-03-03 14:21:32 -0600

    removed disk mod

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,44 +8,44 @@ terraform {
 }
 
 provider "vsphere" {
-  user           = "your-vcenter-user"
-  password       = "your-password"
-  vsphere_server = "vermeervc.vermeermfg.com"
-
-  allow_unverified_ssl = true
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = var.allow_unverified_ssl
 }
 
 data "vsphere_datacenter" "dc" {
-  name = "YourDatacenter"
+  name = var.datacenter
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "YourDatastore"
+  name          = var.datastore
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_compute_cluster" "cluster" {
-  name          = "YourCluster"
+  name          = var.cluster
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_network" "network" {
-  name          = "YourPortGroup"
+  name          = var.network
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "YourWindowsTemplate"
+  name          = var.template_name
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "WINTEST01"
+  name             = var.vm_name
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
+  folder           = var.vm_folder
 
-  num_cpus = 2
-  memory   = 4096
+  num_cpus = var.cpu
+  memory   = var.memory_mb
   guest_id = data.vsphere_virtual_machine.template.guest_id
 
   network_interface {
@@ -53,12 +53,7 @@ resource "vsphere_virtual_machine" "vm" {
     adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
   }
 
-  disk {
-    label            = "disk0"
-    size             = data.vsphere_virtual_machine.template.disks[0].size
-    thin_provisioned = true
-  }
-
+  
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
   }
