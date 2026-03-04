05d489ac200e1d758516b593e24ddfd9c80cff02
lcarkhuff <138237342+lcarkhuff@users.noreply.github.com>
2026-03-04 07:11:36 -0600

    added IP Address and name variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,37 +4,53 @@
 variable "vsphere_server" { type = string }
 variable "vsphere_user" { type = string }
 
-variable "vsphere_password" { 
-  type = string
-  sensitive = true 
+variable "vsphere_password" {
+  type      = string
+  sensitive = true
 }
 
-variable "allow_unverified_ssl" { 
-  type = bool
-  default = true 
+variable "allow_unverified_ssl" {
+  type    = bool
+  default = true
 }
 
 variable "datacenter" { type = string }
-variable "cluster"    { type = string }
-variable "datastore"  { type = string }
-variable "network"    { type = string }
+variable "cluster" { type = string }
+variable "datastore" { type = string }
+variable "network" { type = string }
 
 variable "template_name" { type = string }
 
-variable "vm_name"    { type = string }
-variable "vm_folder"  { type = string }
+variable "vm_name" { type = string }
+variable "vm_folder" { type = string }
 
-variable "cpu"       { 
-  type = number
-  default = 2 
+variable "cpu" {
+  type    = number
+  default = 2
 }
 
 variable "memory_mb" {
-  type = number
+  type    = number
   default = 4096
 }
 
-variable "disk_gb"   {
-  type = number
+variable "disk_gb" {
+  type    = number
   default = null
+}
+
+variable "ip_address" {
+  type = string
+}
+
+variable "subnet_mask" {
+  type = number
+}
+
+variable "gateway" {
+  type = string
+}
+
+variable "dns_servers" {
+  type = list(string)
 }
